68fa389807e23dabce623d941ed95d887be9e7b2
holgergt19 <holger.garcia.tenesaca@udla.edu.ec>
2024-07-15 15:20:51 -0500

    treintaDOS commit

--- tita/settings.py
+++ tita/settings.py
@@ -207,6 +207,6 @@ AWS_STORAGE_BUCKET_NAME = 'bucketstatico'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
